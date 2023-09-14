d7665b4ee424a7ed8d3806e306890924455bb874
Pascal De Wit <Pascaldewit@outlook.com>
2023-09-15 00:59:11 +0200

    Final touches to C01
    • Changed title
    • Simplified CSS
    • Added a type check to arrayToUpperCaseString
    •  Shortened the bg-color conditional using ternary operators

--- C01/index.php
+++ C01/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Software Language 1</title>
+    <title>C01</title>
     <style>
         * {
             margin: 0;
@@ -14,26 +14,18 @@
 
         body {
             height: 100vh;
-            display: grid;
-            place-items: center;
-            background-color: #121212;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             font-family: sans-serif;
-            color: #fff;
         }
 
         table {
-            background-color: #212121;
-            border: 1px solid #f3cc30;
-            border-radius: 0.5rem;
-            border-spacing: 0;
-            overflow: hidden;
-        }
-
-        tr:nth-child(odd) {
-            background-color: #333;
+            border-collapse: collapse;
         }
 
         td {
+            border: 1px solid;
             padding: 0.5rem;
         }
     </style>
@@ -46,26 +38,18 @@
     $pi = round(pi(), 5);
     $married = true;
     $array = ["PhP", "RuST", "gO", "JaVaScRiPt", "PyThOn"];
-    ?>
-    <?php
     function arrayToUpperCaseString($array)
     {
-        $array = array_map('strtolower', $array);
-        $array = array_map('ucfirst', $array);
-        return implode(" ", $array);
+        if (!is_array($array)) {
+            return "Not an array";
+        } else {
+            $array = array_map('strtolower', $array);
+            $array = array_map('ucfirst', $array);
+            return implode(" ", $array);
+        }
     };
-    ?>
-    <?php
     date_default_timezone_set("Europe/Amsterdam");
-    if (date("a") == "am") {
-        echo "<style>body{background-color: #e4d8b4;color:#000;}</style>";
-        echo "<style>table{background-color: #ffbc79;}</style>";
-        echo "<style>tr:nth-child(odd){background-color: #efa96c;}</style>";
-    } else {
-        echo "<style>body{background-color: #130b30;color:#fff;}</style>";
-        echo "<style>table{background-color: #0c071d;}</style>";
-        echo "<style>tr:nth-child(odd){background-color: #1b1043;}</style>";
-    }
+    echo date("a") == "am" ? "<style>body{background-color:#fff;color:#000;}</style>" : "<style>body{background-color:#000;color:#fff;}</style>";
     ?>
     <table>
         <?php
